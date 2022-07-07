f5751dbf11e92fe0ebe23b0b1d82e4eda6706cac
SpectreDev-007 <mohammad.galaxy07@gmail.com>
2022-07-07 15:33:06 +0100

    build Gamespace

--- config/common.mk
+++ config/common.mk
@@ -227,6 +227,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Game Space
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Repainter integration
 PRODUCT_PACKAGES += \
     RepainterServicePriv
